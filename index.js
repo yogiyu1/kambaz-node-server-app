cc5ef7caddd077370e3f67e632c38bc5cc2ba708
yogiyu1002 <yu.xiaoj@northeastern.edu>
2025-04-01 23:41:05 -0700

    Assignments and Assignments Editor

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 import UserRoutes from './Kambaz/Users/routes.js';
 import CourseRoutes from './Kambaz/Courses/routes.js';
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
-// import AssignmentRoutes from './Kambaz/Assignment/routes.js';
+import AssignmentRoutes from './Kambaz/Assignment/routes.js';
 const app = express()
 app.use(cors({
     credentials: true,
@@ -34,5 +34,5 @@ Lab5(app)
 UserRoutes(app)
 CourseRoutes(app)
 ModuleRoutes(app);
-// AssignmentRoutes(app);
+AssignmentRoutes(app);
 app.listen(process.env.PORT || 4000)
